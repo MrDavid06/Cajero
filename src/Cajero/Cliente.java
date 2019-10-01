4c3fbf8b1c2cae35a0d3032562980e41249c136f
David <raymond-06moramontero@hotmail.com>
2019-10-01 00:17:34 -0400

    Primera actualización

---
+++ src/Cajero/Cliente.java
@@ -0,0 +1,29 @@
+package Cajero;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author David
+ */
+public class Cliente {
+    private String creditCard;
+    private int password;
+    public int balance;
+    
+    public static void withdraw(){
+        System.out.println();
+    }
+    
+    public static void viewBalance(){
+    
+    }
+    public static void depositMoney(){
+    
+    }
+}
