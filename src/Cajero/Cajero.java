2ac135dd181d24bde9c07d77ee9b7f35f23f8059
David <raymond-06moramontero@hotmail.com>
2019-10-02 20:50:23 -0400

    Añadí nuevas cosas al Cajero.class

--- src/Cajero/Cajero.java
+++ src/Cajero/Cajero.java
@@ -13,13 +13,17 @@ import java.util.Scanner;
  * @author DELL
  */
 public class Cajero {
-    Login lg1 = new Login();
-    Cliente cli1 = new Cliente();
     
     public static void main(String[] args) {
-
+        Cliente cli1 = new Cliente();
+        Login lg1 = new Login();
+        Cuenta cnt1 = new Cuenta();
+        
         Scanner option = new Scanner(System.in);
+        Scanner option1  = new Scanner(System.in);
         int selection;
+        String transact;
+        
         
         System.out.print("--------");
         System.out.print(" Bienvenido ");
@@ -42,7 +46,36 @@ public class Cajero {
         System.out.println("    3.Deposito de efectivo");
         System.out.println("    4.Salir");
         selection = option.nextInt();
+            }
         
+        if (selection == 1){
+            cnt1.viewBalance();
         }
-    }    
-}
+        else if (selection == 2){
+            cnt1.withdraw();
+          }
+        else if (selection == 3){
+            cnt1.depositMoney();
+        }
+        else if (selection == 4){
+            System.exit(selection);
+            }
+        
+            System.out.println("");
+            System.out.println("------------------------------");
+            System.out.println("¿Desea hacer algo más? S/N");
+            transact = option1.nextLine();
+            System.out.println("------------------------------");
+            
+                if (transact == "S" || transact == "s") {    
+                System.out.println("Por favor seleccione una opcion");
+                System.out.println("    1.Consulta de Saldo");
+                System.out.println("    2.Retiro de efectivo");
+                System.out.println("    3.Deposito de efectivo");
+                System.out.println("    4.Salir");
+                selection = option.nextInt();
+            }
+        }
+    }
+              
+ 
--- src/Cajero/Cuenta.java
+++ src/Cajero/Cuenta.java
@@ -1,5 +1,7 @@
 package Cajero;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,21 +10,37 @@ package Cajero;
 
 /**
  *
- * @author DELL
+ * @author David
  */
+
 public class Cuenta {
     private String creditCard;
     private int password;
     public int balance;
+    Scanner sc = new Scanner(System.in);
     
     public void withdraw(){
-        System.out.println("");
-    }
+        System.out.println("¿Cuánto dinero desea retirar?");
+        
+      }
     
     public void viewBalance(){
+      balance = 0;
+      if (balance == 0){
+          System.out.println("------------------------------");
+          System.out.println("Su balance actual es igual a "+balance);
+          System.out.println("------------------------------");
+      }
+      
+          else{
+            System.out.println("------------------------------");
+            System.out.println("Su balance actual es igual a "+balance);     
+            System.out.println("------------------------------");
+            }
         }
     
     public void depositMoney(){
-    
+        System.out.println("¿Cuánto dinero quiere depositar?");
+        balance = sc.nextInt();
     }
 }
